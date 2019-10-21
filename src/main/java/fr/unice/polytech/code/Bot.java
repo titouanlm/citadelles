a0a088fa7414d0bcecb4586d3646e46eb04c4904
Titouan <titouan.le-mao@etu.unice.fr>
2019-10-21 20:14:34 +0200

    Intégration de la représentation de joueur (bot) et de leur ville respective #4

--- src/main/java/fr/unice/polytech/code/Bot.java
+++ src/main/java/fr/unice/polytech/code/Bot.java
@@ -1,4 +1,69 @@
 package fr.unice.polytech.code;
 
+import java.util.ArrayList;
+
 public class Bot {
+    private String nom;
+    private int nbPiece;
+    private ArrayList<CarteCitadelles> cartesCitadellesEnMain;
+    private Ville villeDuBot;
+    private Personnage personnageACeTour;
+    private boolean possedeCouronne;
+
+    public Bot(String nom) {
+        this.nom = nom;
+        this.nbPiece = 0;
+        this.cartesCitadellesEnMain = new ArrayList<>();
+        this.villeDuBot = new Ville();
+        this.personnageACeTour = null;
+        this.possedeCouronne = false;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getNbPiece() {
+        return nbPiece;
+    }
+
+    public void ajouterPiece(int nbPiece) {
+        this.nbPiece += nbPiece;
+    }
+
+    public void retirerPiece(int nbPiece) {
+        if(this.nbPiece-nbPiece <0){
+            this.nbPiece=0;
+        }else{
+            this.nbPiece-=nbPiece;
+        }
+    }
+
+    public Ville getVilleDuBot() {
+        return villeDuBot;
+    }
+
+    public ArrayList<CarteCitadelles> getCartesCitadellesEnMain() {
+        return cartesCitadellesEnMain;
+    }
+
+    public void ajouterCartesCitadellesDansMain(CarteCitadelles cartesCitadellesAAjouter) {
+        this.cartesCitadellesEnMain.add(cartesCitadellesAAjouter);
+    }
+
+    public Personnage getPersonnageACeTour() {
+        return personnageACeTour;
+    }
+
+    public void setPersonnageACeTour(Personnage personnageACeTour) {
+        this.personnageACeTour = personnageACeTour;
+    }
+
+    public boolean possedeCouronne() {
+        return possedeCouronne;
+    }
+
+    public void setPossedeCouronne(boolean possedeCouronne) {
+        this.possedeCouronne = possedeCouronne;
+    }
 }
--- src/main/java/fr/unice/polytech/code/Ville.java
+++ src/main/java/fr/unice/polytech/code/Ville.java
@@ -1,4 +1,34 @@
 package fr.unice.polytech.code;
 
+import java.util.ArrayList;
+
 public class Ville {
+
+    private ArrayList<CarteCitadelles> batimentsConstruits = new ArrayList<>();
+    private int nbBatimentsConstruits;
+    private int nbTotalPoint;
+
+    Ville(){
+        nbBatimentsConstruits = 0;
+        nbTotalPoint=0;
+    }
+
+    public ArrayList<CarteCitadelles> getBatimentsConstruits() {
+        return batimentsConstruits;
+    }
+
+    public void construireBatiment(CarteCitadelles batimentAConstruire) {
+        this.batimentsConstruits.add(batimentAConstruire);
+        this.nbTotalPoint+=batimentAConstruire.getPoint();
+        this.nbBatimentsConstruits+=1;
+    }
+
+    public int getNbBatimentsConstruits() {
+        return nbBatimentsConstruits;
+    }
+
+    public int getNbTotalPoint() {
+        return nbTotalPoint;
+    }
+
 }
