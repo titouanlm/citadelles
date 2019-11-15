2d2d1b192fe24486dfcc9009eb767e9d6e2c82a4
hamzaayoub98 <hamza.ayoub@etu.univ-cotedazur.fr>
2019-11-15 15:16:57 +0100

    Choisit la carte citadelles la plus utile pour lui entre les deux qu'il a pioché #15

--- src/main/java/fr/unice/polytech/code/BotIntelligent.java
+++ src/main/java/fr/unice/polytech/code/BotIntelligent.java
@@ -4,6 +4,8 @@ import fr.unice.polytech.code.personnages.Architecte;
 import fr.unice.polytech.code.pioches.PiocheCartesCitadelles;
 import fr.unice.polytech.code.pioches.PiocheCartesPersonnage;
 
+import java.util.ArrayList;
+
 public class BotIntelligent extends Bot {
 
     public BotIntelligent(String nom, String couleur) {
@@ -26,7 +28,82 @@ public class BotIntelligent extends Bot {
         if (this.determinerChoixPiocherOuPiece(piocheCartesCitadelles) == 1) {
             this.ajouterPiece(2);
         } else {
-            this.ajouterCartesCitadellesDansMain(piocheCartesCitadelles.piocher());// Peut être déterminée par le joueur
+            ArrayList<CarteCitadelles> cartesCitadellesIntermedieaire = new ArrayList<>();
+
+            cartesCitadellesIntermedieaire.add(piocheCartesCitadelles.piocher());// Peut être déterminée par le joueur
+            cartesCitadellesIntermedieaire.add(piocheCartesCitadelles.piocher());
+
+            CarteCitadelles carteAchoisir;
+
+            String couleur="";
+            ArrayList<String> couleursVilleDuBot = new ArrayList<>();
+            couleursVilleDuBot.add("JAUNE");
+            couleursVilleDuBot.add("BLEU");
+            couleursVilleDuBot.add("VERT");
+            couleursVilleDuBot.add("ROUGE");
+            couleursVilleDuBot.add("VIOLET");
+
+            int j=0;
+            int b=0;
+            int v=0;
+            int r=0;
+            int vi=0;
+            int max=j;
+            int x=0;
+            ArrayList<Integer> nbrCouleursVilleDuBot = new ArrayList<Integer>();
+            nbrCouleursVilleDuBot.add(j);
+            nbrCouleursVilleDuBot.add(b);
+            nbrCouleursVilleDuBot.add(v);
+            nbrCouleursVilleDuBot.add(r);
+            nbrCouleursVilleDuBot.add(vi);
+
+            for(CarteCitadelles batimentsConstruits :this.getVilleDuBot().getBatimentsConstruits()){
+                if(batimentsConstruits.getCouleur()==CouleurCarteCitadelles.JAUNE){
+                    j++;
+                }
+                if(batimentsConstruits.getCouleur()==CouleurCarteCitadelles.BLEU){
+                    b++;
+                }
+                if(batimentsConstruits.getCouleur()==CouleurCarteCitadelles.VERT){
+                    v++;
+                }
+                if(batimentsConstruits.getCouleur()==CouleurCarteCitadelles.ROUGE){
+                    r++;
+                }
+                if(batimentsConstruits.getCouleur()==CouleurCarteCitadelles.VIOLET){
+                    vi++;
+                }
+            }
+            for (int i=0;i<4;i++){
+                if (nbrCouleursVilleDuBot.get(i)< nbrCouleursVilleDuBot.get(i + 1)){
+                    max=nbrCouleursVilleDuBot.get(i);
+                    x=0;
+                }
+            }
+            for (int i=0;i<4;i++){
+                if(x==i){
+                    couleur=couleursVilleDuBot.get(i);
+                }
+            }
+
+            for (CarteCitadelles carte:cartesCitadellesIntermedieaire){
+                if (max>1){
+                    if (carte.getCouleur().equals(couleur)){
+                        carteAchoisir=carte;
+                    }
+                }
+                else {
+                    if(cartesCitadellesIntermedieaire.get(0).getPoint()<cartesCitadellesIntermedieaire.get(1).getPoint()){
+                        if(this.getVilleDuBot().getNbBatimentsConstruits()==7){
+                            carteAchoisir=cartesCitadellesIntermedieaire.get(0);
+                        }
+                        else {
+                            carteAchoisir = cartesCitadellesIntermedieaire.get(1);
+                        }
+                    }
+                    carteAchoisir=cartesCitadellesIntermedieaire.get(0);
+                }
+            }
         }
     }
 
