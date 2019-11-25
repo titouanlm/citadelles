1f713a0b01b5cf37c737efc179d023c5407fbb9b
Julien N'diaye <julienn.ndiaye59155@gmail.com>
2019-11-25 20:29:37 +0100

    Ajout des packages

--- src/main/java/fr/unice/polytech/code/cartes/CarteCitadellesAvecPouvoir.java
+++ src/main/java/fr/unice/polytech/code/cartes/CarteCitadellesAvecPouvoir.java
@@ -1,5 +1,8 @@
 package fr.unice.polytech.code.cartes;
 
+import fr.unice.polytech.code.bots.Bot;
+import fr.unice.polytech.code.pioches.PiocheCartesCitadelles;
+
 public abstract class CarteCitadellesAvecPouvoir extends CarteCitadelles {
     public CarteCitadellesAvecPouvoir(int numero, CouleurCarteCitadelles couleur, String nom, int point) {
         super(numero, couleur, nom, point);
